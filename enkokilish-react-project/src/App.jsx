343c011da7d53383398b98187b52d17bf9dee94e
Helina Ashenafi <Consci210@gmail.com>
2023-03-20 01:12:34 +0300

    Added license

--- enkokilish-react-project/src/App.jsx
+++ enkokilish-react-project/src/App.jsx
@@ -3,13 +3,14 @@ import MathQuestions from "./components/MathQuestions";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
-
+import TopicList from "./components/TopicList";
 function App() {
 
     return (
       <div className="app">
           <Header/>
-          <HomePage />
+          <TopicList/>
+          {/* <HomePage /> */}
           {/* <MathQuestions /> */}
           <Footer/>
       </div>
---
+++ enkokilish-react-project/src/components/TopicList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopicList = () => {
+  return (
+    <div>TopicList</div>
+  )
+}
+
+export default TopicList
