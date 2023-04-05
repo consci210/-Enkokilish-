7ef2fcb9bc6e7647cd23596be45951a4b7c51e69
Helina Ashenafi <Consci210@gmail.com>
2023-04-05 15:15:40 +0300

    Fully functional Login and Signup form

--- enkokilish-react-project/src/config/firebase.js
+++ enkokilish-react-project/src/config/firebase.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Import functions needed 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
+// my firebase Config 
 const firebaseConfig = {
   apiKey: "AIzaSyBfKaX6xswPpwD7Fkc2Ws4t48wnLctTajY",
   authDomain: "enkokilish-9fe30.firebaseapp.com",
@@ -19,6 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
